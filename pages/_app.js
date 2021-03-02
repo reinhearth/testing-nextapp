2c24c0b5c35a76b690040d5b36ca2092bedd8c8b
Raphael Vazquez <raphaelvmdev@gmail.com>
2021-03-01 23:57:53 -0600

    feat: init next app

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import App from 'next/app';
+import 'bootstrap/dist/css/bootstrap.css';
+import '../styles/globals.scss';
+
+export default App;
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+const index = () => {
+  return (
+    <div className="row backred">
+      <div className="col-6">Hola</div>
+      <div className="col-6">mundo</div>
+    </div>
+  );
+};
+
+export default index;
