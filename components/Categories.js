819cc13d6327f8024653fa2679fe07e2587dbed4
Raphael Vazquez <raphaelvmdev@gmail.com>
2021-03-09 08:51:48 -0600

    feat: adding jest setup

--- components/Categories.js
+++ components/Categories.js
@@ -2,9 +2,7 @@ function Categories(props) {
   return (
     <div className="container">
       <div className="row">
-        <p className="mc-1 route-text">
-          {props.categories.join(' > ')}
-        </p>
+        <p className="mc-1 route-text">{props.categories.join(' > ')}</p>
       </div>
     </div>
   );
---
+++ test/components/Categories.spec.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line no-unused-vars
+import Categories from '../../components/Categories';
+import { render } from '@testing-library/react';
+
+describe('Categories Component', () => {
+  let expectedProps;
+  beforeEach(() => {
+    expectedProps = {
+      categories: ['Electronicos', 'Celulares'],
+    };
+  });
+
+  test('Should Render categories path', async () => {
+    const { container  } = render(<Categories {...expectedProps} />);
+    const categories = container.querySelector('p')
+    expect(categories.textContent).toEqual('Electronicos > Celulares');
+  });
+});
