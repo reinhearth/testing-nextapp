9df8c5447c2d398a0c464232a2b94b51f6190dc1
Raphael Vazquez <raphaelvmdev@gmail.com>
2021-03-09 18:55:29 -0600

    adding test url

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ const index = () => {
     <div className="not-found">
       Prueba tecnica para Mercado Libre, Aplicacion construida en NextJS, Inicie
       una busqueda!
+      {process.env.VERCEL_URL}
     </div>
   );
 };
