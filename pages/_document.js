f62664f897c912b1c9ffafe256aacbbba0fc247a
Raphael Vazquez <raphaelvmdev@gmail.com>
2021-03-09 20:12:48 -0600

    feat: adding favicon and title

---
+++ pages/_document.js
@@ -0,0 +1,27 @@
+// eslint-disable-next-line no-unused-vars
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta title="MercadoLibre Test" />
+          <link rel="icon" sizes="96x96" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
